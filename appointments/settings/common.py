0540918d11235b80747d504867b8824a0969d43c
Andrew T. Baker <abaker@twilio.com>
2015-06-08 17:59:08 -0400

    Tweaking datepicker, specifying timezone

--- appointments/settings/common.py
+++ appointments/settings/common.py
@@ -21,17 +21,21 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'not-so-secret'
 
-# Twilio API settings
+# Most important settings
+
+# Twilio API
 TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
 
-# Celery settings
+# Celery
 BROKER_URL = 'redis://localhost:6379/0'
 
 # Reminder time: how early text messages are sent in advance of appointments
 REMINDER_TIME = 30 # minutes
 
-ALLOWED_HOSTS = []
+# Time zone (replace with yours)
+TIME_ZONE = 'America/New_York'
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -104,8 +108,6 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
