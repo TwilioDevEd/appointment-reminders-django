ab22b0eddae31a11db7e25d95761dfa5a0a99f65
Andrew T. Baker <abaker@twilio.com>
2015-06-08 20:00:25 -0400

    Defaulting DEBUG to False

--- appointments/settings/common.py
+++ appointments/settings/common.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'not-so-secret'
